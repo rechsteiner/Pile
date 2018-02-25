4d128aaaa11f01c861e65ce88382fcbd63008590
Martin Rechsteiner <marrechsteiner@gmail.com>
2018-02-25 16:18:34 +0100

    Upgrade to Swift 4.0

--- AnimatedStackView/AnimationMetric.swift
+++ AnimatedStackView/AnimationMetric.swift
@@ -9,10 +9,18 @@ public protocol AnimationMetric {
   var options: UIViewAnimationOptions { get }
 }
 
-struct DefaultAnimationMetric: AnimationMetric {
-  let duration: CFTimeInterval = 0.6
-  let delay: CFTimeInterval = 0
-  let damping: CGFloat = 0.6
-  let initialVelocity: CGFloat = 0
-  let options = UIViewAnimationOptions.beginFromCurrentState
+public struct DefaultAnimationMetric: AnimationMetric {
+  public let duration: CFTimeInterval
+  public let delay: CFTimeInterval
+  public let damping: CGFloat
+  public let initialVelocity: CGFloat
+  public let options: UIViewAnimationOptions
+  
+  public init() {
+    self.duration = 0.6
+    self.delay = 0
+    self.damping = 0.6
+    self.initialVelocity = 0
+    self.options = .beginFromCurrentState
+  }
 }
